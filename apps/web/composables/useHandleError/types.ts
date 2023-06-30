973c13c0b94f8edc5716adf80cc00624aec21171
ljedrasik <ljedrasik@vuestorefront.io>
2023-06-30 13:27:59 +0200

    chore: test

--- apps/web/composables/useHandleError/types.ts
+++ apps/web/composables/useHandleError/types.ts
@@ -1,9 +1,11 @@
 import { H3Error } from 'h3';
 import type { Maybe } from '~/types';
 
-export type ErrorParams = Maybe<H3Error> & {
-  status?: number;
-  statusText?: string;
-};
+export type ErrorParams = Maybe<
+  Partial<H3Error> & {
+    status?: number;
+    statusText?: string;
+  }
+>;
 
 export type UseHandleError = (error: ErrorParams) => void;

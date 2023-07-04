770f846106a813a7eb2f25e91cfab8a4bf09239e
Paweł Orzechowski <porzechowski@vuestorefront.io>
2023-07-04 11:45:28 +0200

    test(storefront-1439): implement PR comments

--- apps/web/__tests__/support/commands.ts
+++ apps/web/__tests__/support/commands.ts
@@ -1,13 +1,14 @@
 declare namespace Cypress {
     interface Chainable {
         getByTestId(id: string): Chainable;
+
         getFixture(id: string): Chainable
     }
 }
 
-Cypress.Commands.add('getByTestId', (testid: string) =>
-    cy.get(`[data-testid="${testid}"]`)
-);
+Cypress.Commands.add('getByTestId', (testId: string) => {
+    cy.get(`[data-testid="${testId}"]`);
+});
 
 Cypress.Commands.add('getFixture', (fixtureName: string) => {
     return cy.fixture(`../fixtures/${fixtureName}.json`).then((fixture) => {

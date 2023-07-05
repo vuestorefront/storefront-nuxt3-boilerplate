f451a9308da1dd34119ab8ba5397d29225976ba8
ljedrasik <ljedrasik@vuestorefront.io>
2023-07-05 08:31:31 +0200

    chore: revert changes

--- apps/web/.eslintrc.cjs
+++ apps/web/.eslintrc.cjs
@@ -5,7 +5,7 @@ module.exports = {
     tsconfigRootDir: __dirname,
   },
   rules: {
-    'max-len': 'off',
+    'max-len': ['warn', { code: 120, ignoreStrings: true, ignoreUrls: true, ignoreTemplateLiterals: true }],
     'max-lines-per-function': 'off',
     'unicorn/no-keyword-prefix': ['error', { disallowedPrefixes: ['new', 'for'] }],
     '@typescript-eslint/no-empty-function': ['error', { allow: ['arrowFunctions'] }],

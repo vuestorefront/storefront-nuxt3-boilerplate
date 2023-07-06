da77d2000d55759b5a1d86ab09370a7c5661fde3
ljedrasik <ljedrasik@vuestorefront.io>
2023-07-06 16:29:05 +0200

    chore: remove unnecessary div

--- apps/web/layouts/checkout.vue
+++ apps/web/layouts/checkout.vue
@@ -1,40 +1,38 @@
 <template>
-  <div>
-    <UiNavbarTop />
-    <main data-testid="checkout-layout">
-      <NarrowContainer>
-        <div class="px-4 md:px-0 mb-20">
-          <div class="flex justify-between mt-8 mb-10">
-            <h1 class="font-bold typography-headline-3 md:typography-headline-2">{{ heading }}</h1>
-            <SfButton
-              :tag="NuxtLink"
-              :href="backHref"
-              class="flex md:hidden whitespace-nowrap"
-              size="sm"
-              variant="tertiary"
-            >
-              <template #prefix>
-                <SfIconArrowBack />
-              </template>
-              {{ backLabelMobile }}
-            </SfButton>
-            <SfButton :tag="NuxtLink" :href="backHref" class="hidden md:flex" variant="tertiary">
-              <template #prefix>
-                <SfIconArrowBack />
-              </template>
-              {{ backLabelDesktop }}
-            </SfButton>
-          </div>
-
-          <span class="!flex justify-center my-40 h-24" v-if="isLoading && !cart">
-            <SfLoaderCircular size="3xl" />
-          </span>
-          <slot v-else />
+  <UiNavbarTop />
+  <main data-testid="checkout-layout">
+    <NarrowContainer>
+      <div class="px-4 md:px-0 mb-20">
+        <div class="flex justify-between mt-8 mb-10">
+          <h1 class="font-bold typography-headline-3 md:typography-headline-2">{{ heading }}</h1>
+          <SfButton
+            :tag="NuxtLink"
+            :href="backHref"
+            class="flex md:hidden whitespace-nowrap"
+            size="sm"
+            variant="tertiary"
+          >
+            <template #prefix>
+              <SfIconArrowBack />
+            </template>
+            {{ backLabelMobile }}
+          </SfButton>
+          <SfButton :tag="NuxtLink" :href="backHref" class="hidden md:flex" variant="tertiary">
+            <template #prefix>
+              <SfIconArrowBack />
+            </template>
+            {{ backLabelDesktop }}
+          </SfButton>
         </div>
-      </NarrowContainer>
-    </main>
-    <UiFooter />
-  </div>
+
+        <span class="!flex justify-center my-40 h-24" v-if="isLoading && !cart">
+          <SfLoaderCircular size="3xl" />
+        </span>
+        <slot v-else />
+      </div>
+    </NarrowContainer>
+  </main>
+  <UiFooter />
 </template>
 
 <script lang="ts" setup>

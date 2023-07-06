dcc3ba66623c2e608a76868764a59377ffa1c070
ljedrasik <ljedrasik@vuestorefront.io>
2023-07-06 16:18:41 +0200

    chore: remove unnecessary div

--- apps/web/pages/checkout.vue
+++ apps/web/pages/checkout.vue
@@ -1,9 +1,7 @@
 <template>
-  <div>
-    <NuxtLayout name="checkout" :back-href="paths.cart" :back-label="$t('back')" :heading="$t('checkout')">
-      This is the checkout page.
-    </NuxtLayout>
-  </div>
+  <NuxtLayout name="checkout" :back-href="paths.cart" :back-label="$t('back')" :heading="$t('checkout')">
+    This is the checkout page.
+  </NuxtLayout>
 </template>
 
 <script lang="ts" setup>

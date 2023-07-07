243ab85eaa0ad6689fcf3a74ff9926ef84161b28
ljedrasik <ljedrasik@vuestorefront.io>
2023-07-07 16:56:41 +0200

    chore: fix

--- apps/web/components/ui/QuantitySelector/QuantitySelector.vue
+++ apps/web/components/ui/QuantitySelector/QuantitySelector.vue
@@ -53,8 +53,10 @@ const { value, minValue, maxValue } = withDefaults(defineProps<QuantitySelectorP
 
 const inputId = useId();
 const { count, inc, dec, set } = useCounter(value);
-const inputClasses =
-  'appearance-none mx-2 w-8 text-center bg-transparent font-medium [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-inner-spin-button]:display-none [&::-webkit-inner-spin-button]:m-0 [&::-webkit-outer-spin-button]:display-none [&::-webkit-outer-spin-button]:m-0 [-moz-appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none disabled:placeholder-disabled-900 focus-visible:outline focus-visible:outline-offset focus-visible:rounded-sm';
+const inputClasses = computed(
+  () =>
+    'appearance-none mx-2 w-8 text-center bg-transparent font-medium [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-inner-spin-button]:display-none [&::-webkit-inner-spin-button]:m-0 [&::-webkit-outer-spin-button]:display-none [&::-webkit-outer-spin-button]:m-0 [-moz-appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none disabled:placeholder-disabled-900 focus-visible:outline focus-visible:outline-offset focus-visible:rounded-sm',
+);
 
 const handleOnChange = (event: Event) => {
   const currentValue = (event.target as HTMLInputElement)?.value;

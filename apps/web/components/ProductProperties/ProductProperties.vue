e8f8c0f1a4c2f85686ec48e8966fc21cd6d4a1d5
Szymon Dziewoński <szymon.dziewonski@gmail.com>
2023-07-12 15:05:30 +0200

    Update apps/web/components/ProductProperties/ProductProperties.vue
    Co-authored-by: Jakub Freisler <kubaef@gmail.com>

--- apps/web/components/ProductProperties/ProductProperties.vue
+++ apps/web/components/ProductProperties/ProductProperties.vue
@@ -2,7 +2,7 @@
   <div class="px-4">
     <span class="block mb-2 mt-2 text-base font-medium leading-6 text-neutral-900">Size</span>
     <div class="mr-2 mb-2 uppercase inline-block">
-      <SfChip class="min-w-[48px]" size="sm"> Label </SfChip>
+      <SfChip class="min-w-[48px]" size="sm">Label</SfChip>
     </div>
     <span class="block mb-2 mt-2 text-base font-medium leading-6 text-neutral-900">Color</span>
     <div class="mr-2 mb-2 inline-block">

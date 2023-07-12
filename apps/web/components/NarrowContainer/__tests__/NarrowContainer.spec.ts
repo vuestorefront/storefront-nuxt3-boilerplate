80c3f71aa5f2210173d125f148bac50aac835316
Szymon Dziewoński <szymon.dziewonski@gmail.com>
2023-07-12 13:43:18 +0200

    chore: CR fixes test

--- apps/web/components/NarrowContainer/__tests__/NarrowContainer.spec.ts
+++ apps/web/components/NarrowContainer/__tests__/NarrowContainer.spec.ts
@@ -1,22 +1,10 @@
 import { VueWrapper, mount } from '@vue/test-utils';
 import NarrowContainer from '~/components/NarrowContainer/NarrowContainer.vue';
 
-const getContainer = (wrapper: VueWrapper) => wrapper.getByTestId('narrow-container');
 describe('<NarrowContainer />', () => {
   it('should render component', () => {
     const wrapper = mount(NarrowContainer);
 
-    expect(getContainer(wrapper));
-  });
-
-  it('should pass children to slot', async () => {
-    const childrenContent = 'some cool children';
-    const wrapper = mount(NarrowContainer, {
-      slots: {
-        default: () => childrenContent,
-      },
-    });
-
-    expect(getContainer(wrapper).text()).to.be.equal(childrenContent);
+    expect(wrapper.getByTestId('narrow-container').exists()).toBeTruthy();
   });
 });

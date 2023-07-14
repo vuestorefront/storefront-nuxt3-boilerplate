4253c94fc1550edaf68232563c72fc1e9d2ee51d
Dawid Ziobro <dziobro@vuestorefront.io>
2023-07-14 13:34:48 +0200

    test: add missing test

--- apps/web/components/ui/Pagination/Pagination.vue
+++ apps/web/components/ui/Pagination/Pagination.vue
@@ -1,5 +1,10 @@
 <template>
-  <nav class="flex justify-between items-end border-t border-neutral-200" role="navigation" aria-label="pagination">
+  <nav
+    class="flex justify-between items-end border-t border-neutral-200"
+    role="navigation"
+    aria-label="pagination"
+    data-testid="pagination"
+  >
     <SfButton
       type="button"
       size="lg"

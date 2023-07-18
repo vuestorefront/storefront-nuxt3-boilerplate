24be98cf23b18531a32e1026bd9a9f01336b6e80
Szymon Dziewoński <szymon.dziewonski@gmail.com>
2023-07-18 12:37:49 +0200

    feat: add ProductRecommended and useProductRecommended

---
+++ apps/web/components/RecommendedProducts/RecommendedProducts.vue
@@ -0,0 +1,12 @@
+<template>
+  <p data-testid="recommended-products" class="my-4 typography-text-lg font-body">
+    {{ $t('recommendedProducts') }}
+  </p>
+  <ProductSlider :items="products" />
+</template>
+
+<script setup lang="ts">
+import type { RecommendedProductsProps } from '~/components/RecommendedProducts/types';
+
+defineProps<RecommendedProductsProps>();
+</script>

140c792efd984724139e76b69853fccfff4b81af
Dawid Ziobro <dziobro@vuestorefront.io>
2023-07-26 15:44:54 +0200

    fix: sonarcloud issues

--- apps/web/components/CheckoutPayment/types.ts
+++ apps/web/components/CheckoutPayment/types.ts
@@ -9,6 +9,4 @@ export type CheckoutPaymentProps = {
   activePayment: string;
 };
 
-export type CheckoutPaymentEmits = {
-  (event: 'update:activePayment', parameter: PaymentMethod): void;
-};
+export type CheckoutPaymentEmits = (event: 'update:activePayment', parameter: PaymentMethod) => void;

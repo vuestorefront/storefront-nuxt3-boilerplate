3ef4ca1e46b5919f61c260e77b442c12e6eb65c6
Szymon Dziewoński <szymon.dziewonski@gmail.com>
2023-08-07 13:16:48 +0200

    chore: CR changes

--- apps/web/pages/signup.vue
+++ apps/web/pages/signup.vue
@@ -20,20 +20,20 @@
       @submit.prevent="open"
     >
       <label>
-        <UiFormLabel>{{ $t('form.firstNameLabel') }}</UiFormLabel>
+        <UiFormLabel>{{ $t('form.firstNameLabel') }} *</UiFormLabel>
         <SfInput name="firstName" autocomplete="given-name" v-model="firstNameModel" required />
       </label>
       <label>
-        <UiFormLabel>{{ $t('form.lastNameLabel') }}</UiFormLabel>
+        <UiFormLabel>{{ $t('form.lastNameLabel') }} *</UiFormLabel>
         <SfInput name="lastName" autocomplete="family-name" v-model="lastNameModel" required />
       </label>
       <label>
-        <UiFormLabel>{{ $t('form.emailLabel') }}</UiFormLabel>
+        <UiFormLabel>{{ $t('form.emailLabel') }} *</UiFormLabel>
         <SfInput name="email" type="email" autocomplete="email" v-model="emailModel" required />
       </label>
       <div>
         <label>
-          <UiFormLabel>{{ $t('form.passwordLabel') }}</UiFormLabel>
+          <UiFormLabel>{{ $t('form.passwordLabel') }} *</UiFormLabel>
           <UiFormPasswordInput name="password" autocomplete="current-password" v-model="passwordModel" required />
           <UiFormHelperText class="mb-2">{{ $t('form.passwordHint') }}</UiFormHelperText>
         </label>
@@ -42,7 +42,7 @@
       <div class="flex items-center">
         <SfCheckbox id="terms" v-model="termsAndConditionsModel" value="value" class="peer" required />
         <label
-          class="ml-3 text-base text-gray-900 cursor-pointer font-body peer-disabled:text-disabled-900"
+          class="ml-3 text-base text-neutral-900 cursor-pointer font-body peer-disabled:text-disabled-900"
           for="terms"
         >
           *
@@ -62,7 +62,7 @@
       <div class="flex mb-2">
         <SfCheckbox id="subscription" v-model="subscriptionsModel" value="value" class="peer mt-0.5" />
         <label
-          class="ml-3 text-base text-gray-900 cursor-pointer font-body peer-disabled:text-disabled-900"
+          class="ml-3 text-base text-neutral-900 cursor-pointer font-body peer-disabled:text-disabled-900"
           for="subscription"
         >
           {{ $t('form.subscriptionLabel') }}
@@ -83,7 +83,7 @@
         tag="section"
         role="alertdialog"
         disable-click-away
-        aria-labelledby="signUpModealTitle"
+        aria-labelledby="signUpModalTitle"
         aria-describedby="signUpModalDesc"
       >
         <header class="flex items-center flex-col">
@@ -94,7 +94,7 @@
             height="192"
             class="my-6"
           />
-          <h2 id="signUpModealTitle" class="mt-6 mb-4 font-bold typography-headline-4 md:typography-headline-3">
+          <h2 id="signUpModalTitle" class="mt-6 mb-4 font-bold typography-headline-4 md:typography-headline-3">
             {{ $t('auth.signup.modal.heading') }}
           </h2>
         </header>
@@ -102,7 +102,8 @@
           <i18n-t keypath="auth.signup.modal.description" tag="p" id="signUpModalDesc">
             <template #information>
               <SfLink
-                href="#"
+                :tag="NuxtLink"
+                :to="paths.account"
                 class="focus:outline focus:outline-offset-2 focus:outline-2 outline-secondary-600 rounded"
               >
                 {{ $t('auth.signup.modal.information') }}

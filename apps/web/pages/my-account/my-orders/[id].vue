ce96c0a34f7d7b8cf1fa996a63c663172619e420
Justyna <jgieracka@vuestorefront.io>
2023-08-10 12:45:06 +0200

    feat: order details

--- apps/web/pages/my-account/my-orders/[id].vue
+++ apps/web/pages/my-account/my-orders/[id].vue
@@ -4,17 +4,21 @@
       v-model="isOpen"
       as="section"
       role="dialog"
-      class="h-full w-full overflow-auto md:h-fit max-h-[100%] md:max-w-[770px] !px-0 min-w-650px"
+      class="h-full w-full overflow-hidden md:h-fit max-h-[100%] md:max-w-[770px] min-w-650px !relative !p-4 md:!p-10"
     >
-      <header class="fixed w-full pl-4 pr-2 top-0 py-2 z-10 bg-white items-center typography-headline-4 font-bold">
-        <div class="flex justify-between">
-          <h1 class="my-2">{{ $t('account.ordersAndReturns.orderDetails.heading') }}</h1>
-          <SfButton square variant="tertiary" :tag="NuxtLink" :to="paths.accountMyOrders">
-            <SfIconClose class="text-neutral-500" />
-          </SfButton>
-        </div>
+      <header class="flex justify-between bg-white items-center typography-headline-4 font-bold">
+        <h3>{{ $t('account.ordersAndReturns.orderDetails.heading') }}</h3>
+        <SfButton
+          square
+          variant="tertiary"
+          :tag="NuxtLink"
+          :to="paths.accountMyOrders"
+          class="md:absolute md:top-2 md:right-2"
+        >
+          <SfIconClose class="text-neutral-500" />
+        </SfButton>
       </header>
-      <main class="mt-10 px-4 md:px-10">
+      <main class="mt-6 md:px-4 max-h-[591px] overflow-auto">
         <ul class="bg-neutral-100 p-4 rounded-md md:columns-2 mb-6">
           <li>
             <p class="font-medium">{{ $t('account.ordersAndReturns.orderDetails.orderId') }}</p>
@@ -38,11 +42,11 @@
           <ul class="flex">
             <li class="flex-grow pr-4 py-4">
               <p class="typography-text-sm font-medium">{{ $t('account.ordersAndReturns.orderDetails.price') }}</p>
-              <span>$295.87</span>
+              <span>${{ product.price }}</span>
             </li>
             <li class="flex-grow p-4">
               <p class="typography-text-sm font-medium">{{ $t('account.ordersAndReturns.orderDetails.quantity') }}</p>
-              <span>1</span>
+              <span>{{ product.quantity }}</span>
             </li>
             <li class="flex-[50%] justify-self-end text-right pl-4 py-4">
               <p class="typography-text-sm font-medium">{{ $t('account.ordersAndReturns.orderDetails.subtotal') }}</p>
@@ -67,16 +71,16 @@
             </tr>
           </thead>
           <tbody>
-            <tr v-for="(product, i) in products" :key="i" class="border-b border-neutral-200">
+            <tr v-for="(product, i) in products" :key="i" class="border-b border-neutral-200 align-top">
               <td class="pb-4 pr-4 lg:whitespace-nowrap"><UiProductCardVertical :product="product" /></td>
-              <td class="p-4 lg:whitespace-nowrap">$295.87</td>
-              <td class="p-4">1</td>
+              <td class="p-4 lg:whitespace-nowrap">${{ product.price }}</td>
+              <td class="p-4">{{ product.quantity }}</td>
               <td class="p-4">$295.87</td>
             </tr>
           </tbody>
         </table>
 
-        <div class="flex justify-between border-t pt-4 border-neutral-200">
+        <div class="flex justify-between pt-4 border-t border-neutral-200 md:border-0">
           <p>{{ $t('account.ordersAndReturns.orderDetails.itemsSubtotal') }}</p>
           <span>${{ data?.summary.subtotal }}</span>
         </div>
@@ -159,6 +163,8 @@ const NuxtLink = resolveComponent('NuxtLink');
 const products = [
   {
     name: 'Smartwatch Fitness Tracker',
+    quantity: 1,
+    price: '295.87',
     gallery: [
       {
         alt: 'Smartwatch Fitness Tracker',
@@ -168,20 +174,22 @@ const products = [
     attributes: [
       {
         label: 'Size',
-        name: 'size',
-        value: '1.9',
+        name: 'Size',
+        value: '1.9″',
         valueLabel: 'value',
       },
       {
         name: 'Color',
         label: 'color',
-        value: 'Red',
+        value: 'Black',
         valueLabel: 'value',
       },
     ],
   },
   {
     name: 'Smartwatch Fitness Tracker',
+    quantity: 1,
+    price: '295.87',
     gallery: [
       {
         alt: 'Smartwatch Fitness Tracker',
@@ -191,14 +199,14 @@ const products = [
     attributes: [
       {
         label: 'Size',
-        name: 'size',
-        value: '1.9',
+        name: 'Size',
+        value: '1.9″',
         valueLabel: 'value',
       },
       {
         name: 'Color',
         label: 'color',
-        value: 'Red',
+        value: 'Black',
         valueLabel: 'value',
       },
     ],

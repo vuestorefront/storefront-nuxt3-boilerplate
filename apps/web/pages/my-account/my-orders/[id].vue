8496b910c5149ca1602f786c750508e30c450bdd
Justyna <jgieracka@vuestorefront.io>
2023-08-10 11:24:39 +0200

    feat: order details

--- apps/web/pages/my-account/my-orders/[id].vue
+++ apps/web/pages/my-account/my-orders/[id].vue
@@ -1,10 +1,132 @@
 <template>
   <UiOverlay visible>
-    <SfModal v-model="isOpen">
-      <SfButton square variant="tertiary" class="absolute right-2 top-2" :tag="NuxtLink" :to="paths.accountMyOrders">
-        <SfIconClose />
-      </SfButton>
-      {{ data }}
+    <SfModal
+      v-model="isOpen"
+      as="section"
+      role="dialog"
+      class="h-full w-full overflow-auto md:h-fit max-h-[100%] md:max-w-[770px] !px-0 min-w-650px"
+    >
+      <header class="fixed w-full pl-4 pr-2 top-0 py-2 z-10 bg-white items-center typography-headline-4 font-bold">
+        <div class="flex justify-between">
+          <h1 class="my-2">{{ $t('account.ordersAndReturns.orderDetails.heading') }}</h1>
+          <SfButton square variant="tertiary" :tag="NuxtLink" :to="paths.accountMyOrders">
+            <SfIconClose class="text-neutral-500" />
+          </SfButton>
+        </div>
+      </header>
+      <main class="mt-10 px-4 md:px-10">
+        <ul class="bg-neutral-100 p-4 rounded-md md:columns-2 mb-6">
+          <li>
+            <p class="font-medium">{{ $t('account.ordersAndReturns.orderDetails.orderId') }}</p>
+            <span>{{ data?.id }}</span>
+          </li>
+          <li class="my-4 md:mb-0">
+            <p class="font-medium">{{ $t('account.ordersAndReturns.orderDetails.orderDate') }}</p>
+            <span>{{ data?.date }}</span>
+          </li>
+          <li>
+            <p class="font-medium">{{ $t('account.ordersAndReturns.orderDetails.paymentAmount') }}</p>
+            <span>{{ data?.paymentAmount }}</span>
+          </li>
+          <li class="mt-4">
+            <p class="font-medium">{{ $t('account.ordersAndReturns.orderDetails.status') }}</p>
+            <span>{{ data?.status }}</span>
+          </li>
+        </ul>
+        <div v-for="(product, i) in products" :key="i" class="md:hidden">
+          <UiProductCardVertical :product="product" />
+          <ul class="flex">
+            <li class="flex-grow pr-4 py-4">
+              <p class="typography-text-sm font-medium">{{ $t('account.ordersAndReturns.orderDetails.price') }}</p>
+              <span>$295.87</span>
+            </li>
+            <li class="flex-grow p-4">
+              <p class="typography-text-sm font-medium">{{ $t('account.ordersAndReturns.orderDetails.quantity') }}</p>
+              <span>1</span>
+            </li>
+            <li class="flex-[50%] justify-self-end text-right pl-4 py-4">
+              <p class="typography-text-sm font-medium">{{ $t('account.ordersAndReturns.orderDetails.subtotal') }}</p>
+              <span>$295.87</span>
+            </li>
+          </ul>
+        </div>
+        <table class="hidden md:table w-full text-left typography-text-sm mx-4 md:mx-0">
+          <caption class="hidden">
+            {{
+              $t('account.ordersAndReturns.orderDetails.tableCaption')
+            }}
+          </caption>
+          <thead>
+            <tr class="border-b-2 border-neutral-200">
+              <th class="py-3 font-medium">{{ $t('account.ordersAndReturns.orderDetails.product') }}</th>
+              <th class="py-3 px-4 font-medium lg:whitespace-nowrap">
+                {{ $t('account.ordersAndReturns.orderDetails.price') }}
+              </th>
+              <th class="py-3 px-4 font-medium">{{ $t('account.ordersAndReturns.orderDetails.quantity') }}</th>
+              <th class="py-3 px-4 font-medium">{{ $t('account.ordersAndReturns.orderDetails.subtotal') }}</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(product, i) in products" :key="i" class="border-b border-neutral-200">
+              <td class="pb-4 pr-4 lg:whitespace-nowrap"><UiProductCardVertical :product="product" /></td>
+              <td class="p-4 lg:whitespace-nowrap">$295.87</td>
+              <td class="p-4">1</td>
+              <td class="p-4">$295.87</td>
+            </tr>
+          </tbody>
+        </table>
+
+        <div class="flex justify-between border-t pt-4 border-neutral-200">
+          <p>{{ $t('account.ordersAndReturns.orderDetails.itemsSubtotal') }}</p>
+          <span>${{ data?.summary.subtotal }}</span>
+        </div>
+        <div class="flex justify-between my-2">
+          <p>{{ $t('account.ordersAndReturns.orderDetails.delivery') }}</p>
+          <span>{{ data?.summary.delivery }}</span>
+        </div>
+        <div class="flex justify-between border-b pb-4 border-neutral-200">
+          <p>{{ $t('account.ordersAndReturns.orderDetails.estimatedTax') }}</p>
+          <span>${{ data?.summary.estimatedTax }}</span>
+        </div>
+        <div class="flex justify-between border-b py-4 border-neutral-200 typography-text-lg font-medium">
+          <p>{{ $t('account.ordersAndReturns.orderDetails.total') }}</p>
+          <span>${{ data?.summary.total }}</span>
+        </div>
+        <ul class="md:columns-2 mt-6">
+          <li class="mb-4">
+            <p class="typography-text-sm font-medium mb-2">
+              {{ $t('account.ordersAndReturns.orderDetails.billingAddress') }}
+            </p>
+            <span>{{ data?.billingAddress.firstName }}</span>
+            <span>{{ data?.billingAddress.lastName }}</span>
+            <span class="block">{{ data?.billingAddress.phoneNumber }}</span>
+            <span>{{ data?.billingAddress.postalCode }}</span>
+            <span class="block">{{ data?.billingAddress.city }}</span>
+          </li>
+          <li class="mb-4">
+            <p class="typography-text-sm font-medium mb-2">
+              {{ $t('account.ordersAndReturns.orderDetails.paymentMethod') }}
+            </p>
+            <span>{{ data?.paymentMethod }}</span>
+          </li>
+          <li class="mb-4">
+            <p class="typography-text-sm font-medium mb-2">
+              {{ $t('account.ordersAndReturns.orderDetails.shippingAddress') }}
+            </p>
+            <span>{{ data?.shippingAddress.firstName }}</span>
+            <span>{{ data?.shippingAddress.lastName }}</span>
+            <span class="block">{{ data?.shippingAddress.phoneNumber }}</span>
+            <span>{{ data?.shippingAddress.postalCode }}</span>
+            <span class="block">{{ data?.shippingAddress.city }}</span>
+          </li>
+          <li class="mb-4">
+            <p class="typography-text-sm font-medium mb-2">
+              {{ $t('account.ordersAndReturns.orderDetails.paymentMethod') }}
+            </p>
+            <span>{{ data?.shipping }}</span>
+          </li>
+        </ul>
+      </main>
     </SfModal>
   </UiOverlay>
 </template>
@@ -33,4 +155,53 @@ watch(
 );
 
 const NuxtLink = resolveComponent('NuxtLink');
+
+const products = [
+  {
+    name: 'Smartwatch Fitness Tracker',
+    gallery: [
+      {
+        alt: 'Smartwatch Fitness Tracker',
+        url: '/images/watch.png',
+      },
+    ],
+    attributes: [
+      {
+        label: 'Size',
+        name: 'size',
+        value: '1.9',
+        valueLabel: 'value',
+      },
+      {
+        name: 'Color',
+        label: 'color',
+        value: 'Red',
+        valueLabel: 'value',
+      },
+    ],
+  },
+  {
+    name: 'Smartwatch Fitness Tracker',
+    gallery: [
+      {
+        alt: 'Smartwatch Fitness Tracker',
+        url: '/images/watch.png',
+      },
+    ],
+    attributes: [
+      {
+        label: 'Size',
+        name: 'size',
+        value: '1.9',
+        valueLabel: 'value',
+      },
+      {
+        name: 'Color',
+        label: 'color',
+        value: 'Red',
+        valueLabel: 'value',
+      },
+    ],
+  },
+];
 </script>

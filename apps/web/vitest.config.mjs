216901b98d295b044a760f028643dd6988baef25
Szymon Dziewoński <szymon.dziewonski@gmail.com>
2023-09-15 16:38:53 +0200

    chore: add type

--- apps/web/vitest.config.mjs
+++ apps/web/vitest.config.mjs
@@ -3,6 +3,7 @@ import { defineVitestConfig } from 'nuxt-vitest/config';
 export default defineVitestConfig({
   test: {
     environment: 'nuxt',
+    clearMocks: true,
     globals: true,
     setupFiles: './vue-test-utils.extend.js',
     include: ['**/*/?(*.)+(spec|test).[jt]s'],

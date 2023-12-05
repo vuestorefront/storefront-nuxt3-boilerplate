3bfae142042f15421e35257c35581c7a8fd63a92
Bedeoan Raul <raul.bedeoan@plentymarkets.com>
2023-12-05 14:45:41 +0200

    feat: add coupon compsable

--- apps/web/composables/useCart/types.ts
+++ apps/web/composables/useCart/types.ts
@@ -6,12 +6,14 @@ export interface UseCartState {
   loading: boolean;
 }
 
+export type ApplyCouponToCart = (coupon: string) => Promise<Ref<Maybe<SfCart>>>;
 export type FetchCart = () => Promise<Ref<Maybe<SfCart>>>;
 
 export interface UseCart {
   data: Readonly<Ref<UseCartState['data']>>;
   loading: Readonly<Ref<boolean>>;
   fetchCart: FetchCart;
+  applyCouponToCart: ApplyCouponToCart;
 }
 
 export type UseCartReturn = () => UseCart;
